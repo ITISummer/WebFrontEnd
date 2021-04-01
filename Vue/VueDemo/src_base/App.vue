95d0af12e0f7b88df956e17f2e9fc9e12a75524f
summer <lv.summer@qq.com>
2021-04-01 22:01:20 +0800

    something new

---
+++ Vue/VueDemo/src_base/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <img src="./assets/logo.png" alt="logo" class="logo">
+    <HelloWorld/> //加载 HelloWorld 组件的内容
+  </div>
+</template>
+
+<script>
+  import HelloWorld from './components/HelloWorld.vue'
+
+  export default {
+    components: {
+      HelloWorld
+    }
+  }
+</script>
+
+<style>
+  .logo {
+    width: 100px;
+    height: 100px;
+  }
+</style>
---
+++ Vue/VueDemo/src_base/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2 class="title">{{msg}}</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () { //组件里 data 属性必须是一个函数
+      return {
+        msg: '第一个Vue组件'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .title {
+    color: red;
+    background: yellow;
+  }
+</style>
